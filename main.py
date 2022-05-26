1050db58f93bbb9b9b75d70b8ed8f66708f8f34d
ConnorGit123 <80865252+ConnorGit123@users.noreply.github.com>
2022-05-26 23:47:21 +0000

    put some comments

---
+++ main.py
@@ -0,0 +1,30 @@
+# import statements
+
+
+# fuctions go here
+
+
+# ********** Main Routine **********
+
+# Set up ditcionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (cant be blank)
+
+    # Get age (between 12 and 130
+
+    # Calculate ticket price 
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # ask for payment method (and apply surcharge if necessary)
+
+
+# Calculate total sales and profit
+
+# Output data to text file
