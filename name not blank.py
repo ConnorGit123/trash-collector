95386a9e17724b91b034d517d1eb5b408cba43da
ConnorGit123 <80865252+ConnorGit123@users.noreply.github.com>
2022-06-08 23:16:51 +0000

    changed the error message and added a loop

--- name not blank.py
+++ name not blank.py
@@ -1,5 +1,3 @@
-# import statements
-
 # Fuctions go here
 
 def not_blank(question):
@@ -23,35 +21,3 @@ def not_blank(question):
 # Set up ditcionaries / lists needed to hold data
 
 # Ask user if they have used the program before & show instructions if necessary
-
-# fuctions go here
-
-# checks that ticket name is not blank
-
-# Loop to get ticket details
-name = ""
-count = 0
-MAX_TICKETS = 4
-
-while name != "xxx" and count <= MAX_TICKETS:
-
-    # Get details...
-    name = input("Name: ")
-    count += 1
-    # Get name (cant be blank)
-    
-
-    # Get age (between 12 and 130
-
-    # Calculate ticket price 
-
-    # Loop to ask for snacks
-
-    # Calculate snack price
-
-    # ask for payment method (and apply surcharge if necessary)
-
-
-# Calculate total sales and profit
-
-# Output data to text file
